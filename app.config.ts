a243f4bd515631a52c5b1cedea8f14e06511ae79
Ben,Nwogbo <ben.nwogbo@usfoods.com>
2025-06-29 01:32:52 -0500

    working expo

--- app.config.ts
+++ app.config.ts
@@ -9,30 +9,39 @@ export default {
     slug: 'deliverease',
     version: '1.0.0',
     orientation: 'portrait',
-    icon: './assets/icon.png',
+    icon: './assets/images/icon.png',
     userInterfaceStyle: 'light',
     splash: {
-      image: './assets/icon.png',
+      image: './assets/images/splash.png',
       resizeMode: 'contain',
       backgroundColor: '#ffffff'
     },
     assetBundlePatterns: ['**/*'],
     ios: {
       supportsTablet: true,
-      bundleIdentifier: 'com.yourcompany.deliverease'
+      bundleIdentifier: 'com.bistra.deliverease',
+      googleServicesFile: './GoogleService-Info.plist',
+      deploymentTarget: '15.1',
+      infoPlist: {
+        ITSAppUsesNonExemptEncryption: false
+      }
     },
     android: {
       adaptiveIcon: {
-        foregroundImage: './assets/adaptive-icon.png',
+        foregroundImage: './assets/images/icon.png',
         backgroundColor: '#ffffff'
       },
-      package: 'com.yourcompany.deliverease'
+      package: 'com.bistra.deliverease',
+      googleServicesFile: './google-services.json'
     },
     web: {
-      favicon: './assets/favicon.png'
+      favicon: './assets/images/favicon.png'
     },
     newArchEnabled: true,
     extra: {
+      eas: {
+        projectId: "454e0c29-ae41-4f52-9fc5-f6ea119e454d"
+      },
       firebaseApiKey: process.env.FIREBASE_API_KEY,
       firebaseAuthDomain: process.env.FIREBASE_AUTH_DOMAIN,
       firebaseProjectId: process.env.FIREBASE_PROJECT_ID,
@@ -45,7 +54,18 @@ export default {
       '@react-native-firebase/app',
       '@react-native-firebase/auth',
       'expo-location',
-      'expo-image-picker'
+      'expo-image-picker',
+      [
+        'expo-build-properties',
+        {
+          ios: {
+            useFrameworks: 'dynamic',
+            deploymentTarget: '15.1',
+            ccacheEnabled: false,
+            flipper: false
+          }
+        }
+      ]
     ]
   }
 }; 
